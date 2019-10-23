d2e04a487a6057422dc5db68954826907018c271
Reynard van den Berg <reyvdb@gmail.com>
2019-10-23 11:59:41 +0800

    add contact page along with controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   #by using get we arrange for the route to respond to a GET request
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  root 'application#hello'
 end
